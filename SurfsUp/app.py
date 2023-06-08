bff8a36ca9383f65cc8db2653387273a39931f86
Wheezyotter <jacobpohs@gmail.com>
2023-06-08 11:56:54 -0700

    Added comments

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -11,14 +11,15 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-# reflect an existing database into a new model
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflects hawaii.sqlite database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", 
+                       connect_args={'check_same_thread': False})
 base = automap_base()
 
-# reflect the tables
+# reflects the tables
 base.prepare(autoload_with=engine)
 
-# Save references to each table
+# Save references to each table, measurement and station
 measurement = base.classes.measurement
 station = base.classes.station
 
@@ -30,38 +31,43 @@ session = Session(engine)
 #################################################
 app = Flask(__name__)
 
-
 #################################################
 # Flask Routes
 #################################################
+
+# Home page - gives list of possible routes
 @app.route("/")
 def index():
     return (
             f"Available Routes:"
             f"<ul>"
-            f"<li>Precipitation : /api/v1.0/precipitation</li>"
-            f"<li>/api/v1.0/stations</li>"
-            f"<li>/api/v1.0/tobs</li>"
-            f"<li>/api/v1.0/<start></li>"
-            f"<li>/api/v1.0/<start>/<end></li>"
+            f"<li>Precipitation : <a href='http://127.0.0.1:5000/api/v1.0/precipitation' target='_blank'> /api/v1.0/precipitation </a></li>"
+            f"<li>Stations: <a href='http://127.0.0.1:5000/api/v1.0/stations' target='_blank'> /api/v1.0/stations</a></li>"
+            f"<li>Temperature: <a href='http://127.0.0.1:5000/api/v1.0/tobs' target='_blank'> /api/v1.0/tobs</a></li>"
+            f"<li>Insert Start Date: /api/v1.0/<start></li>"
+            f"<li>Insert Date Range: /api/v1.0/<start>/<end></li>"
             f"</ul>"
             )
 
+# Precipitation - gives latest year precipitation data in JSON format
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-
+    # Finds the most recent date in the data
     latest_date = session.query(measurement.date).\
                         order_by(measurement.date.desc()).\
                         first()
     
     # Converts latest date to datetime format
     latest_date_dt = dt.datetime.strptime(latest_date[0], '%Y-%m-%d').date()
+    # Finds the date exactly one year from the latest date
     query_date = latest_date_dt - dt.timedelta(weeks = 52.2)
 
+    # Queries the date and precipitation from the latest year in descending order
     latest_year_data =  session.query(measurement.date, measurement.prcp).\
     filter(measurement.date >= query_date).\
     order_by(measurement.date.desc()).all()
 
+    # Formats data into a dictionary
     latest_year_precip = []
     for date, prcp in latest_year_data:
         dates_precip = {}
@@ -69,10 +75,13 @@ def precipitation():
         dates_precip["prcp"] = prcp
         latest_year_precip.append(dates_precip)
 
+    # Returns in json format
     return jsonify(latest_year_precip)
 
+# Stations - gives all distinct stations in json format
 @app.route("/api/v1.0/stations")
 def stations():
+    # Queries all station information
     all_stations_query = session.query(station.id, 
                                        station.station, 
                                        station.name,
@@ -81,6 +90,7 @@ def stations():
                                        station.elevation).\
                         all()
 
+    # Formats data into a dictionary
     all_stations = []
     for id, stn, name, lat, lng, elev in all_stations_query:
         stations_dict = {}
@@ -90,33 +100,37 @@ def stations():
         stations_dict["lat"] = lat
         stations_dict["long"] = lng
         stations_dict["elevation"] = elev
-
         all_stations.append(stations_dict)
 
+    # Returns in json format
     return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-
-    latest_date = session.query(measurement.date).\
+    # Finds the most recent date in the data
+    latest_date_temp = session.query(measurement.date).\
                         order_by(measurement.date.desc()).\
                         first()
     
     # Converts latest date to datetime format
-    latest_date_dt = dt.datetime.strptime(latest_date[0], '%Y-%m-%d').date()
-    query_date = latest_date_dt - dt.timedelta(weeks = 52.2)
+    latest_date_temp_dt = dt.datetime.strptime(latest_date_temp[0], '%Y-%m-%d').date()
+    # Finds the date exactly one year from the latest date
+    query_date_temp = latest_date_temp_dt - dt.timedelta(weeks = 52.2)
 
+    # Queries most active stations and the number of measurements per station
     station_query = session.query(measurement.station, func.count(measurement.station)).\
                             group_by(measurement.station).\
                             order_by(func.count(measurement.station).desc()).\
                             all()
 
+    # Queries temperature from the latest year and from the most active station
     latest_year_temp_query =  session.query(measurement.date, measurement.tobs).\
                                 filter(measurement.station == station_query[0][0]).\
-                                filter((measurement.date >= query_date)).\
+                                filter((measurement.date >= query_date_temp)).\
                                 order_by(measurement.date).\
                                 all()
     
+    # Formats data into a dictionary
     latest_year_temp = []
     for date, temp in latest_year_temp_query:
         dates_temp = {}
@@ -124,10 +138,12 @@ def tobs():
         dates_temp["temp"] = temp
         latest_year_temp.append(dates_temp)  
 
+    # Returns in json format
     return jsonify(latest_year_temp)
 
 @app.route("/api/v1.0/<start>")
 def start(start):
+    # Finds the most recent date in the data
     latest_date = session.query(measurement.date).\
                           order_by(measurement.date.desc()).\
                           first()
